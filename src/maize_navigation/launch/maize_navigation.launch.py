b0587cc026d85a862e5bb4e5af84f0c806346a42
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2026-05-17 21:55:16 +0200

    Update maize_navigation.launch.py

--- src/maize_navigation/launch/maize_navigation.launch.py
+++ src/maize_navigation/launch/maize_navigation.launch.py
@@ -1,21 +1,20 @@
+from pathlib import Path
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-import os
 
 
 def generate_launch_description():
-    pkg_share = get_package_share_directory("maize_navigation")
-    params_file = os.path.join(pkg_share, "config", "params.yaml")
-
-    navigator = Node(
-        package="maize_navigation",
-        executable="navigator",
-        name="maize_navigator",
-        output="screen",
-        parameters=[params_file],
-    )
+    pkg_share = Path(get_package_share_directory("maize_navigation"))
+    params_file = pkg_share / "config" / "params.yaml"
 
     return LaunchDescription([
-        navigator,
+        Node(
+            package="maize_navigation",
+            executable="navigator",
+            name="maize_navigator",
+            output="screen",
+            parameters=[str(params_file)],
+        )
     ])
