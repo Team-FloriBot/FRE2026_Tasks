5b3d4c5577882e7890423d5a3b9acc31cba5ad6b
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2026-05-17 20:16:28 +0200

    Update setup.py

--- src/maize_navigation/setup.py
+++ src/maize_navigation/setup.py
@@ -1,33 +1,29 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
-package_name = 'maize_navigation'
+package_name = "maize_navigation"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.1.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/maize_navigation.launch.py']),
-        ('share/' + package_name + '/config', ['config/params.yaml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
+        (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='norika-schneider',
-    maintainer_email='norika.schneider@gmail.com',
-    description='Navigates robot through maize field according to given pattern',
-    license='Apache-2.0',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
-entry_points={
-        'console_scripts': [
-            'navigator = maize_navigation.maize_navigation:main',
-            'laser_merger = maize_navigation.laser_merger:main',
+    maintainer="floribot",
+    maintainer_email="todo@example.com",
+    description="Maize row navigation with local path planning in one ROS 2 node.",
+    license="Apache-2.0",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "navigator = maize_navigation.maize_navigation:main",
         ],
     },
 )
