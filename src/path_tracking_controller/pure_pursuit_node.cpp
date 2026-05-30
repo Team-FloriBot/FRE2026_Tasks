93d53d64f56f1c1dd52e09e55cdd26aaf1d889df
norikaschneider <norikaschneider@gmail.com>
2026-05-31 00:44:36 +0200

    Update marker frame_id to use "odom" in Pure Pursuit Node for consistency

--- src/path_tracking_controller/pure_pursuit_node.cpp
+++ src/path_tracking_controller/pure_pursuit_node.cpp
@@ -233,7 +233,7 @@ private:
                               float r, float g, float b)
         {
             visualization_msgs::msg::Marker m;
-            m.header.frame_id = global_frame_;
+            m.header.frame_id = "odom"";
             m.header.stamp = this->now();
             m.ns = "pp_debug";
             m.id = id;
